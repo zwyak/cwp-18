4da680d634e7315fef73812acf33b24bae712b7c
zwyak <vlad.senyuk97@gmail.com>
2020-03-06 14:43:41 +0300

    Added Logger class

--- logger.js
+++ logger.js
@@ -1,1 +1,15 @@
 const moment = require('moment');
+
+class Logger {
+  constructor(prefix, defaultLevel, dateFormat) {
+    if (!prefix || !defaultLevel || !dateFormat) throw new Error();
+    this.prefix = prefix;
+    this.defaultLevel = defaultLevel;
+    this.dateFormat = dateFormat;
+  }
+
+  format(message, level){
+    const date = moment().format(this.dateFormat);
+    return `${date} | ${this.prefix} | ${!level ? this.defaultLevel : level} | ${message}`
+  }
+}
